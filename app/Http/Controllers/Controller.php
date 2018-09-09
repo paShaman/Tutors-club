72b76529f351f4d54283379c880e5ebb5feaccb1
paShaman <nikitin696@gmail.com>
2018-09-09 20:12:46 +0300

    2018-09-09

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,5 +6,13 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    public $page;
+    public $title = ['Tutors club'];
+
+    public function render()
+    {
+        $data = [];
+
+        return view('pages.' . $this->page, $data);
+    }
 }
